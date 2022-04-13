c2efcabe4a7720a953fe15b3c9ad9ca91f4511b7
Ionut Trif <trifionut207@gmail.com>
2022-04-13 18:42:37 +0300

    Replaced encryption with hash passwords

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app=express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,10 +19,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save(function(err){
@@ -64,7 +58,7 @@ app.post("/login", function(req,res){
             console.log(err);
         }else{
             if (foundUser){
-                if(foundUser.password === password) {
+                if(foundUser.password === md5(password)) {
                     res.render("secrets");
                 }
             }
